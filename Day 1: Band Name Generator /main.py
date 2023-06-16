ac480d9471f799d7d21f3aab786411d6f5e87e50
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-16 18:07:07 +0530

    main.py

--- Day 1: Band Name Generator /main.py
+++ Day 1: Band Name Generator /main.py
@@ -1,1 +1,4 @@
-
+print("Welcome to then Band Name Generator.")
+city_name=input("What is the name of the city you grew up in? \n")
+pet_name=input("What is your pet? \n")
+print("Band Name is "+ city_name +" "+ pet_name +".\n")
