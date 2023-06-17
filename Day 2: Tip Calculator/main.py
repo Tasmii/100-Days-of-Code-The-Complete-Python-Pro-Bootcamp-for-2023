a82be87623a6804b4f7c72da23d4521c707ae729
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-17 17:45:03 +0530

    main.py

--- Day 2: Tip Calculator/main.py
+++ Day 2: Tip Calculator/main.py
@@ -1,1 +1,7 @@
-
+print("Welcome to the Tip Calculator!")
+amount = input("What was the total bill? ")
+percentage = input("What percentage tip would you like to give? ")
+people = input("How many people to split the bill? ")
+multiplier = float(int(percentage)/100) + 1
+tip = ((float(amount)/int(people))*multiplier)
+print("Each person should pay: $"+str(round(tip, 2)))
