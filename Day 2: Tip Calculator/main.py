e9b8ca377c980704251d0ac0f459245968bf3ab3
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-17 17:49:34 +0530

    main.py

--- Day 2: Tip Calculator/main.py
+++ Day 2: Tip Calculator/main.py
@@ -1,3 +1,8 @@
+#This code is a simple tip calculator that calculates the amount each person should pay based on the total bill, the desired tip percentage, and the number of people splitting the bill. 
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
 print("Welcome to the Tip Calculator!")
 amount = input("What was the total bill? ")
 percentage = input("What percentage tip would you like to give? ")
