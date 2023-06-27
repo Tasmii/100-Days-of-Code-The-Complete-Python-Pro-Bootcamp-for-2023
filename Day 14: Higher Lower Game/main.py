235c21421890616503d74bb5632948964ab52950
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-27 21:27:11 +0530

    Create main.py

---
+++ Day 14: Higher Lower Game/main.py
@@ -0,0 +1,67 @@
+#The provided code is a simple "Higher Lower" game that compares the number of followers between two randomly selected social media accounts.
+
+from art import logo, vs
+from data import data
+import random
+import os
+
+def format_data(account):
+  """Takes the account data and returns the printable format."""
+  account_name = account["name"]
+  account_descr = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Take the user guess and follower counts and returns if they got it right."""
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+# Display art
+print(logo)
+score = 0
+game_should_continue = True
+# Generate a random account from the game data.
+account_b = random.choice(data)
+
+# Make the game repeatable.
+while game_should_continue:
+
+  # Making account at position B become the next account at position A.
+  account_a = account_b
+  account_b = random.choice(data)
+
+  if account_a == account_b:
+    account_b = random.choice(data)
+
+  print(f"Compare A: {format_data(account_a)}.")
+  print(vs)
+  print(f"Against B: {format_data(account_b)}.")
+
+  # Ask user for a guess.
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+  # Check if user is correct.
+  ## Get follower count of each account.
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+  # Clear the screen between rounds.
+  os.system('cls')
+  print(logo)
+  
+  # Give user feedback on their guess.
+  # Score keeping.
+  if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+  else:
+    game_should_continue = False
+    print(f"Sorry, that's wrong. Final score: {score}.")
+
+#The code repeats the game loop until the user makes an incorrect guess. 
+#The user is shown two accounts and asked to guess which account has more followers. 
+#The score is incremented for each correct guess, and the game ends when the user guesses incorrectly.
