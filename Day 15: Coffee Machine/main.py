7a5bdc1d55602576d8712a2cef1af1d3a5d243a2
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-29 21:10:42 +0530

    Create main.py

---
+++ Day 15: Coffee Machine/main.py
@@ -0,0 +1,100 @@
+#The provided code is a simple coffee machine program that allows users to order different types of coffee, manages the inventory of resources, handles payment, and tracks profits. 
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+#This function checks if the resources required to make a specific coffee order are available. 
+#It compares the ingredients needed for the order with the available resources. 
+#If any ingredient is insufficient, it prints a message indicating the shortage and returns False. Otherwise, it returns True.
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient."""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"​Sorry there is not enough {item}.")
+            return False
+    return True
+
+#This function handles the payment process by asking the user to input the number of different coins (quarters, dimes, nickels, and pennies) they are inserting. 
+#It calculates the total value of the coins and returns that value.
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+#This function determines whether the payment made by the user is sufficient to cover the cost of the selected coffee. 
+#If the payment is enough, it calculates the change to be given, updates the profit variable, and returns True. Otherwise, it prints a message indicating insufficient payment and returns False.
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+# This function deducts the required ingredients for a coffee order from the available resources. 
+#It updates the resource quantities accordingly and prints a message indicating the successful preparation of the ordered coffee.
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+
+is_on = True
+
+while is_on:
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+#This code can be used as a starting point for building a coffee machine program or as an example to understand basic concepts of managing resources, payments, and inventory in a simple application.
