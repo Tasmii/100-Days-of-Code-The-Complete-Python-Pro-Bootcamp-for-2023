1afdb8c2cf65c3f4b39d4603cb7f4c899cd38d46
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-07-12 22:45:15 +0530

    Update scoreboard.py

--- Day 21: Snake Game Part 2 (Complete)/scoreboard.py
+++ Day 21: Snake Game Part 2 (Complete)/scoreboard.py
@@ -4,6 +4,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("Day 21 Snake Game Part 2\data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -11,13 +13,21 @@ class Scoreboard(Turtle):
         self.hideturtle()
     
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align = "center", font = ("Courier", 20, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align = "center", font = ("Courier", 20, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("Day 21 Snake Game Part 2\data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score+=1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align = "center", font = ("Courier", 20, "normal"))
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write(f"GAME OVER", align = "center", font = ("Courier", 20, "normal"))
