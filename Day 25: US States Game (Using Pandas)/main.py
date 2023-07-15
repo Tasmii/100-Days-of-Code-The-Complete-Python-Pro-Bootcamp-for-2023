79c4e52b06f63f06c76a0c328b212bd383478c53
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-07-15 23:13:18 +0530

    Updated using List Comprehension

--- Day 25: US States Game (Using Pandas)/main.py
+++ Day 25: US States Game (Using Pandas)/main.py
@@ -18,11 +18,12 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        print(missing_states)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        #missing_states = []
+        #for state in all_states:
+        #    if state not in guessed_states:
+        #        missing_states.append(state)
+        #print(missing_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("Day 25 US State Game (CSV Data)/states to learn.csv")
         break
