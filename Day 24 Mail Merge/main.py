c244775c74a9428d77cb9b60cffd93fa24d035e9
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-08-08 23:48:40 +0530

    Update main.py

--- Day 24 Mail Merge/main.py
+++ Day 24 Mail Merge/main.py
@@ -1,3 +1,12 @@
+#This Python code is designed to automate the process of mail merge by generating personalized letters using template files. 
+#It first reads a list of names from the "invited_names.txt" file and stores them in the `names` list. 
+#Then, it opens the "starting_letter.txt" template file, which contains a letter with a placeholder "[name]" to be replaced. 
+#For each name in the `names` list, it strips any whitespace, replaces the placeholder in the template letter with the current name, and creates a new personalized letter. 
+#The new letter content is written to a separate text file named "letter_for_[name].txt" based on the stripped name. 
+#This process generates a set of personalized letters, each addressed to an individual name, by replacing the placeholder with the actual name from the list. 
+#This code facilitates efficient creation of personalized letters for mail merge tasks.
+
+
 PLACEHOLDER = "[name]"
 with open(r"Day 24 Mail Merge\invited_names.txt") as names_file:
     names = names_file.readlines()
